c9445c9c1ae3b0d87fc9d56a4afa7ac228b55487
ZackyNjenga <zacharynjenga381@gmail.com>
2023-08-21 13:28:58 +0300

    This should work

--- _printf.c
+++ _printf.c
@@ -1,45 +1,29 @@
 #include "main.h"
-/**
- * _printf  this is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
- */
-int _printf(const char * const format, ...)
-{
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
 
+/**
+ * _printf -This function produces output according to a format 
+ * @format: is a character string the format string 
+ * consists of azero or more directives 
+ * 
+ * return: Number of characters printed(this excludes 
+ * the null byte used to end output to strings )
+**/
+int _printf(const char *format, ...){
+	int size;
 	va_list args;
-	int i = 0, j, len = 0;
+
+	if (format == NULL)
+	return(-1);
+	size = _strlen(format);
+	if (size <= 0 ) 
+	return (0);
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+	size = handler(format, args);
 
-Here:
-	while (format[i] != '\0')
-	{
-		j = 13;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		len++;
-		i++;
-	}
+	_putchar(-1);
 	va_end(args);
-	return (len);
+
+	return (size )
+
 }
